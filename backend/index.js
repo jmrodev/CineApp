a842473a62c4602cdb085c9fac6b4390d39438f1
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-11-21 21:30:39 -0300

    feature(doc) : add routers

--- backend/index.js
+++ backend/index.js
@@ -5,33 +5,10 @@ const port = 3000;
 
 app.use(cors());
 
-const movies = [
-    {
-        title: 'The Matrix',
-        genre: 'Sci-Fi',
-        schedule: '8:00 PM',
-        room: 'Sala 1',
-        available_seats: 25
-    },
-    {
-        title: 'The Lord of the Rings',
-        genre: 'Fantasy',
-        schedule: '9:00 PM',
-        room: 'Sala 2',
-        available_seats: 20
-    },
-    {
-        title: 'The Godfather',
-        genre: 'Crime',
-        schedule: '10:00 PM',
-        room: 'Sala 3',
-        available_seats: 15
-    }
-];
+const movieRouter = require('./routers/movieRouter');
+app.use('/api', movieRouter);
 
-app.get('/movies', (req, res) => {
-    res.json(movies);
-});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
